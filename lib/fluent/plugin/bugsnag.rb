0fc780432ac576ced70e0282e06e212e173ffdd6
koshigoe <koshigoeb@gmail.com>
2015-04-21 22:56:17 +0900

    initial commit

---
+++ lib/fluent/plugin/bugsnag.rb
@@ -0,0 +1,9 @@
+require "fluent/plugin/bugsnag/version"
+
+module Fluent
+  module Plugin
+    module Bugsnag
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/fluent/plugin/bugsnag_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Fluent::Plugin::Bugsnag do
+  it 'has a version number' do
+    expect(Fluent::Plugin::Bugsnag::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
